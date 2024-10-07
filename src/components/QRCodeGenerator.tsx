3a4d941e5756826e6241b4a8e40b548716dc41d6
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-10-07 14:34:50 +0530

    Added download QR functionality

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -1,17 +1,36 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent,useRef } from 'react';
 import QRCode from 'react-qr-code';
+import { saveAs } from 'file-saver';
+import { toPng } from 'html-to-image';
+
 import { BackgroundLines } from "../components/ui/BackgroundLines"
 
 const QRCodeGenerator: React.FC = () => {
   const [inputText, setInputText] = useState<string>('');
+  const qrRef = useRef<HTMLDivElement>(null);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
     setInputText(e.target.value);
   };
 
+  const handleDownloadClick = () => {
+    if (!qrRef.current) {
+      console.error("QR code reference is null.");
+      return;
+    }
+
+    toPng(qrRef.current)
+      .then((dataUrl) => {
+        saveAs(dataUrl, 'qr-code.png');
+      })
+      .catch((err) => {
+        console.error('Error generating image', err);
+      });
+  };
+
   return (
 
-    <BackgroundLines>
+    <BackgroundLines >
 
     <div className="flex flex-col items-center justify-center h-screen p-4 bg-gray-100">
       <h1 className="text-2xl font-bold mb-4">QR Code Generator</h1>
@@ -21,15 +40,21 @@ const QRCodeGenerator: React.FC = () => {
         placeholder="Enter text or URL"
         value={inputText}
         onChange={handleInputChange}
-        className="p-2 mb-4 border border-gray-400 rounded-md w-full max-w-xs"
+        className="p-2 mb-4 border border-gray-400 rounded-md w-full sm:max-w-xs"
       />
 
       {inputText && (
-        <div className="bg-white p-4 rounded-md shadow-md">
+        <div className="bg-white p-4 rounded-md shadow-md" ref={qrRef}>
           <QRCode value={inputText} />
         </div>
       )}
     </div>
+    <button
+          onClick={handleDownloadClick}
+          className="p-2 bg-blue-500 text-white rounded-md"
+        >
+          Download QR
+        </button>
       </BackgroundLines>
   );
 };
